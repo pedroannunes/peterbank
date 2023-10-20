586c965183e12a6c36fa898445d0447afb61df9c
Pedro Augusto <pedroaugustonnunes@outlook.com>
2023-10-20 11:56:05 -0300

    retificando tamanhos

--- src/componentes/SegundaSection.js
+++ src/componentes/SegundaSection.js
@@ -9,7 +9,7 @@ function SegundaSection (){
             <img src={mulher} className={Style.mulher} data-aos="fade-right" data-aos-duration="3000" />
 
             <div className={Style.caixaTxt} data-aos="fade-right" data-aos-duration="3000">
-                <h1 className={Style.txt3}>Tenha condições <span className={Style.destaque}>futurísticas</span>,as menores <span className={Style.destaque}>taxas</span>, e melhores <span className={Style.destaque}>vantages</span>!</h1>
+                <h1 className={Style.txt3}>Tenha condições <span className={Style.destaque}>futurísticas</span>,as menores <span className={Style.destaque}>taxas</span>, e melhores <span className={Style.destaque}>vantagens</span>!</h1>
 
                 <h2 className={Style.txt4}>Solicite agora mesmo o seu <span className={Style.destaque}>cartão</span> de <span className={Style.destaque}>crédito</span>.</h2>
 
